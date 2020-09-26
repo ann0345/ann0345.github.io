fb19db3dd38fbee633a7e2ab02af5ef0ead7ef53
Pinjung.chen <gino101303@gmail.com>
2020-09-26 14:16:07 +0800

    show pic width

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Pic />
     <Todo />
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   </div>
 </template>
 
 <script>
+import Pic from './components/Pic.vue'
 import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
+    Pic,
     Todo
   }
 }
