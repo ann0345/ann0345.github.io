c4d75f052d8603b8dca96d5d701ee26d46fd4d8e
Pinjung.chen <gino101303@gmail.com>
2020-09-26 11:07:22 +0800

    add todo list

---
+++ src/components/Todo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input v-model="text" />
+    <button @click="add" :disabled="!text">add</button>
+    <button @click="reset">reset list</button>
+    <ul>
+      <li v-for="item in list" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    text: "",
+    list: [],
+  }),
+  methods: {
+    add() {
+      if(this.text){
+        this.list.push(this.text);
+        this.text = "";
+      }
+      else{
+        alert("enter your text");
+      }
+    },
+    reset() {
+      this.list = [];
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
