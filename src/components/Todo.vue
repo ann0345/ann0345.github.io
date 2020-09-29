68c0a96df154ebd3ee05df1fa7b43794a1e19ee4
Pinjung.chen <gino101303@gmail.com>
2020-09-29 10:35:29 +0800

    todo 父子互傳

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,65 +1,39 @@
 <template>
   <div>
-    <input v-model="text" />
-    <span :class="{ red : invalid }">{{ inputLength }}</span>
-    <br/>
-    <button @click="add" :disabled="invalid">add</button>
-    <button @click="reset">reset list</button>
+    <TodoInput @content="getContent" />
     <ul>
       <li
         v-for="item in list"
         :key="item.text"
-        :class="{ deleted: item.done }"
       >
-        <input
-         type="checkbox"
-         v-model="item.done"
-        />
-        {{ item.text }}
+        <input type="checkbox" v-model="item.done">
+          {{ item.text }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import TodoInput from "./TodoInput";
+
 export default {
+  components: { TodoInput },
   data: () => ({
-    text: "",
     list: [],
   }),
   computed:{
-    inputLength(){
-      return this.text.length;
-    },
-    invalid(){
-      return this.inputLength < 0 || this.inputLength > 10;
-    }
   },
   methods: {
-    add() {
-      if(!this.text) return;
-      if(!this.invalid) return;
-      const item ={
-        text: this.text,
-        done: false,
-      };
-        this.list.push(item);
-        this.text = "";
-    },
-    reset() {
-      this.text = "";
-      this.list = [];
+    getContent(text){
+      const item = {
+        text,
+        done : false
+      }
+      this.list.push(item);
     },
   },
 };
 </script>
 
 <style scoped>
-.deleted {
-  color:grey;
-  text-decoration: line-through;
-}
-.red{
-  color:red;
-}
 </style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input v-model="text" />
+    <button @click="add" :disabled="invalid">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    text: "",
+  }),
+  computed:{
+    invalid(){
+      return this.text.length <= 0 || this.text.length > 10;
+    }
+  },
+  methods: {
+    add() {
+      if(this.invalid) return;
+      this.$emit('content', this.text);
+      this.text = "";
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
