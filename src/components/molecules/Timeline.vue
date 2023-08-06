d16e927d37aecf65a9490203bee9a6e2d38e24bf
ann0345 <38250450+ann0345@users.noreply.github.com>
2023-08-06 20:03:18 +0800

    add timeline/ul list components

---
+++ src/components/molecules/Timeline.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-timeline dense>
+      <v-timeline-item v-for="(item, index) in value" :key="item">
+        <v-card class="timelineCard">
+          <v-card-title class="text-h5">
+            {{ item.company }} - {{ item.title }} ({{ item.fromMonth }}-{{
+              item.toMonth
+            }})</v-card-title
+          >
+          <v-card-text class="talign-start">
+            <p class="summery">{{ item.summery }}</p>
+            <span class="detail">{{ item.detail }}</span>
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "TimelineComponent",
+  props: {
+    value: {
+      type: Array,
+    },
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scope>
+.timelineCard {
+  background-color: lightgrey;
+}
+.summery {
+  font-size: 22px;
+}
+.detail {
+  font-size: 20px;
+  padding-left: 20px;
+}
+</style>
---
+++ src/components/molecules/UnorderedList.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <span class="subTitle">{{ subTitle }}</span>
+    <ul>
+      <li v-for="(item, index) in value" :key="item" class="talign-start">
+        <span class="name">{{ item.name }}</span>
+      </li>
+    </ul>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "UnorderedListComponent",
+  props: {
+    subTitle: {
+      type: String,
+    },
+    value: {
+      type: Array,
+    },
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.subTitle {
+  text-align: start !important;
+  font-size: 24px;
+}
+.name {
+  font-size: 20px;
+}
+</style>
